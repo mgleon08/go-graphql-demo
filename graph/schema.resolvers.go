98b97945b8c0883c8b51dd2a02532580ae0e1e69
Leon Ji <leon@shoplineapp.com>
2022-10-26 17:00:29 +0800

    Add Query

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -10,15 +10,43 @@ import (
 	"graphql/graph/model"
 )
 
-// CreateTodo is the resolver for the createTodo field.
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+// CreateLink is the resolver for the createLink field.
+func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
+	panic(fmt.Errorf("not implemented: CreateLink - createLink"))
 }
 
-// Todos is the resolver for the todos field.
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+// CreateUser is the resolver for the createUser field.
+func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
+	panic(fmt.Errorf("not implemented: CreateUser - createUser"))
 }
+
+// Login is the resolver for the login field.
+func (r *mutationResolver) Login(ctx context.Context, input model.Login) (string, error) {
+	panic(fmt.Errorf("not implemented: Login - login"))
+}
+
+// RefreshToken is the resolver for the refreshToken field.
+func (r *mutationResolver) RefreshToken(ctx context.Context, input model.RefreshTokenInput) (string, error) {
+	panic(fmt.Errorf("not implemented: RefreshToken - refreshToken"))
+}
+
+// Links is the resolver for the links field.
+// func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
+// 	panic(fmt.Errorf("not implemented: Links - links"))
+
+// }
+
+func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
+	var links []*model.Link
+	dummyLink := model.Link{
+	  ID: "12345678",
+	  Title: "our dummy link",
+	  Address: "https://address.org",
+	  User: &model.User{ID: "User123", Name: "admin"},
+	}
+	  links = append(links, &dummyLink)
+	  return links, nil
+  }
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
