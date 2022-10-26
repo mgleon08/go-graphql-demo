0caf7f0d9dd69ec28af50083906f11c543fe5495
Leon Ji <leon@shoplineapp.com>
2022-10-26 17:00:45 +0800

    Add Mutation

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -11,8 +11,18 @@ import (
 )
 
 // CreateLink is the resolver for the createLink field.
+// func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
+// 	panic(fmt.Errorf("not implemented: CreateLink - createLink"))
+// }
+
 func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
-	panic(fmt.Errorf("not implemented: CreateLink - createLink"))
+	var link model.Link
+	var user model.User
+	link.Address = input.Address
+	link.Title = input.Title
+	user.Name = "test"
+	link.User = &user
+	return &link, nil
 }
 
 // CreateUser is the resolver for the createUser field.
